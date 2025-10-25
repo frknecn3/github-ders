06f6be03526c8a318c9b56a973289ba184d8ae10
Furkan Ercan <furkanduhannuman@gmail.com>
2025-10-25 18:22:58 +0300

    js commit

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ console.log('Hello World!')
 console.log('Öznurun projesi')
 
 console.log("Bu JavaScript TahaSerdar'a aittir.")
+
+// furkanın eklediği kodlar
+
+console.log('bu js furkana ait, sakın silmeyin, conflict olursa bunu kabul edin')
